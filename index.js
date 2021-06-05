4f75ac7398c7dded82a619cee41ca0b1f864bb6e
pablo <pablo@>
2021-06-04 22:37:01 -0400

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+// js is an non-blocking async programming lenguage
+// event manager called 'event loop'
+// only onw thread for inputs and outputs
+
+// memory heap
+// call stack --> functions to run
+// task queue --> callbacks
+
+// callbacksz
---
+++ src/callback/challenge.js
@@ -0,0 +1,54 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
+
+function fetchData(url_api, callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url_api, true);
+    xhttp.onreadystatechange = function(event) {
+        if (xhttp.readyState === 4) {
+            if (xhttp.status === 200) {
+                callback(null, JSON.parse(xhttp.responseText))
+            } else {
+                const error = new Error('Error' + url_api);
+                return callback(error, null)
+            }
+        }
+    }
+    xhttp.send();
+}
+
+fetchData(API, function(error1, data1) {
+    // console.log(data1);
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2) {
+        // console.log(data2);
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function(error3, data3) {
+            // console.log(data3);
+            if (error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
+
+
+
+
+
+/*
+Property 	        Description
+onreadystatechange 	Defines a function to be called when the readyState property changes
+readyState 	        Holds the status of the XMLHttpRequest.
+                    0: request not initialized
+                    1: server connection established
+                    2: request received
+                    3: processing request
+                    4: request finished and response is ready
+status 	            200: "OK"
+                    403: "Forbidden"
+                    404: "Page not found"
+                    For a complete list go to the Http Messages Reference
+statusText 	        Returns the status-text (e.g. "OK" or "Not Found")
+*/
